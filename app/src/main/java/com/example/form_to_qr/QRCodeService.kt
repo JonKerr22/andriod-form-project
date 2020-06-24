6af95249f43abd65a28348b6585f260e8d6a5b3d
Jonathan Kerr <jonathankerr@MacBook-Pro.local>
2020-06-23 22:27:10 -0500

    reinitialized project as a Java andriod app

--- app/src/main/java/com/example/form_to_qr/QRCodeService.kt
+++
@@ -1,8 +0,0 @@
-package com.example.form_to_qr
-
-class QRCodeService {
-    //two sources for this
-    //https://codinginfinite.com/qrcode-generator-and-reader-android-example/ - in future if need be, give this dude credit
-    //https://stackoverflow.com/questions/29649673/scan-barcode-from-an-image-in-gallery-android
-
-}
